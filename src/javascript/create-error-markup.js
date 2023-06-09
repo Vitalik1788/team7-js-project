67aa9701692959d2a925dafa041d8ea64bd4d673
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-06-09 11:46:16 +0200

    bugfix

--- src/javascript/create-error-markup.js
+++ src/javascript/create-error-markup.js
@@ -3,10 +3,9 @@ import { refs } from './catalog/components/refs';
 let errorMarkupCreated = false;
 
 export const createErrorMarkup = () => {
-  const wrapper = document.querySelector('.pagination-wrapper')
 
   if (errorMarkupCreated) {
-    return; // Если функция уже вызвана, выходим из неё
+    return;
   }
 
   const errorMarkup = `<p class="error-information">OOPS...</p>
@@ -19,7 +18,5 @@ export const createErrorMarkup = () => {
   newErrorContainer.innerHTML = errorMarkup;
 
   refs.errorContainer.appendChild(newErrorContainer);
-  refs.errorContainer.classList.remove('is-hidden');
-  wrapper.classList.add('is-hidden')
   errorMarkupCreated = true;
 };
